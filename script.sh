44dee77074b01f0405dfba52735f64254d1b8e79
subash basnet <subashbasnet0@gmail.com>
2023-04-12 09:43:30 +0545

    added chromedriver script

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ if chromedriver --version
 then
     echo 'yes chromedriver'
 else
+    sudo yum install libxi6 libgconf-2-4
     cd /tmp/
     wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
