b05dcdedbc6778426e75378a96c207a59f8acebc
subash basnet <subashbasnet0@gmail.com>
2023-04-12 09:54:20 +0545

    added chromedriver script

--- script.sh
+++ script.sh
@@ -7,4 +7,8 @@ else
     wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
     unzip chromedriver_linux64.zip
     sudo mv chromedriver /usr/bin/chromedriver
+
+    curl https://intoli.com/install-google-chrome.sh | bash
+    sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+    google-chrome --version && which google-chrome
 fi
